43b822bb41bafdc907144c0d66c2f1667cc1b8f4
Dan Dodd <dan@lutd.io>
2022-03-31 15:42:55 -0500

    restore textbox option

--- resources/js/components/Tool.vue
+++ resources/js/components/Tool.vue
@@ -16,6 +16,7 @@
                         dusk="commenter"
                         rows="5"
                         v-model="comment"
+                        @keyup.enter.93="createComment"
                         >
                     </textarea>
                 </div>
